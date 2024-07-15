b18ae014a1dbc5b8f23546329659d1933d58fb63
izzudd <36723840+izzudd@users.noreply.github.com>
2024-07-15 07:57:39 +0700

    example app: word count

--- hadoop/app/run.sh
+++ hadoop/app/run.sh
@@ -5,5 +5,5 @@ hdfs dfs -put guttenberg/*.txt /guttenberg
 hadoop jar hadoop-streaming.jar \
   -input /guttenberg \
   -output /guttenberg-wc \
-  -mapper "python3 /root/example/mapper.py" \
-  -reducer "python3 /root/example/reducer.py"
+  -mapper "python3 /root/app/mapper.py" \
+  -reducer "python3 /root/app/reducer.py"
